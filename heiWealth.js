9be02bdb12e7c171211e50b17de2fb33cf87d3a7
dchlfin <167969296+dchlfin@users.noreply.github.com>
2026-05-12 23:34:00 +0400

    refactor: arrange data and functions by relative information

--- heiWealth.js
+++ heiWealth.js
@@ -1,11 +1,7 @@
-// initializations
 // toggle nav start
     const btn = document.getElementById('toggle-nav-btn')
     const nav = document.getElementById('nav')
-// toggle nav end
 
-// event listeners
-// toggle nav start
     btn.addEventListener("click", () => {
         nav.classList.toggle("open")
     })
